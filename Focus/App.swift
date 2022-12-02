cb214cde1a08f7a4faeb16e986abd5e3bffb772e
Ole Begemann <ole@oleb.net>
2022-12-02 22:43:16 +0100

    Build simple shape editor, bind Delete menu command to action

--- Focus/App.swift
+++ Focus/App.swift
@@ -6,5 +6,49 @@ struct FocusApp: App {
         WindowGroup {
             ContentView()
         }
+        .commands {
+            MyEditingCommands()
+        }
+    }
+}
+
+struct MyEditingCommands: Commands {
+    @FocusedBinding(\.items) private var items: [Item]?
+    @FocusedBinding(\.selectedItem) private var selectedItem: Item.ID??
+
+    var body: some Commands {
+        CommandGroup(after: .pasteboard) {
+            Button("Delete") {
+                withAnimation {
+                    if let idx = items?.firstIndex(where: { $0.id == selectedItem }) {
+                        items?.remove(at: idx)
+                    }
+                }
+            }
+            .keyboardShortcut(KeyboardShortcut(.delete, modifiers: []))
+            .disabled(selectedItem == .some(nil) || selectedItem == .none)
+        }
+    }
+}
+
+struct SelectedShapeKey: FocusedValueKey {
+    typealias Value = Binding<Item.ID?>
+}
+
+struct ItemsKey: FocusedValueKey {
+    typealias Value = Binding<[Item]>
+}
+
+extension FocusedValues {
+    var selectedItem: Binding<Item.ID?>? {
+        get { self[SelectedShapeKey.self] }
+        set { self[SelectedShapeKey.self] = newValue }
+    }
+}
+
+extension FocusedValues {
+    var items: Binding<[Item]>? {
+        get { self[ItemsKey.self] }
+        set { self[ItemsKey.self] = newValue }
     }
 }
--- Focus/ContentView.swift
+++ Focus/ContentView.swift
@@ -1,14 +1,102 @@
 import SwiftUI
 
+struct Item: Identifiable {
+    var id: UUID = .init()
+    var color: Color = .random()
+    var frame: CGRect = CGRect(
+        x: CGFloat.random(in: 0...300).rounded(),
+        y: .random(in: 0...300).rounded(),
+        width: .random(in: 100...500).rounded(),
+        height: .random(in: 100...500).rounded()
+    )
+}
+
+extension Color {
+    static func random() -> Self {
+        Color(hue: .random(in: 0...1), saturation: 0.9, brightness: 0.9)
+    }
+}
+
+let initialItems: [Item] = [
+    .init(),
+    .init(),
+    .init(),
+    .init(),
+]
+
 struct ContentView: View {
+    @State var items: [Item] = initialItems
+    @State var selectedItem: Item.ID? = nil
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            ScrollView([.horizontal, .vertical]) {
+                ZStack(alignment: .topLeading) {
+                    ForEach(items) { item in
+                        let isSelected = item.id == selectedItem
+                        MyShape(color: item.color, isSelected: isSelected)
+                            .frame(width: item.frame.width, height: item.frame.height)
+                            .position(x: item.frame.midX, y: item.frame.midY)
+                            .onTapGesture {
+                                selectedItem = item.id
+                            }
+
+                    }
+                }
+                .frame(minWidth: geometry.size.width, minHeight: geometry.size.height, alignment: .topLeading)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    selectedItem = nil
+                }
+            }
         }
-        .padding()
+        .toolbar {
+            toolbar
+        }
+        .focusedSceneValue(\.items, $items)
+        .focusedSceneValue(\.selectedItem, $selectedItem)
+    }
+
+    @ToolbarContentBuilder private var toolbar: some ToolbarContent {
+        ToolbarItem {
+            Button {
+                withAnimation {
+                    items.append(Item())
+                }
+            } label: {
+                Label("Add", systemImage: "plus")
+            }
+            .help("Add shape")
+        }
+        ToolbarItem {
+            Button {
+                withAnimation {
+                    if let idx = items.firstIndex(where: { $0.id == selectedItem }) {
+                        items.remove(at: idx)
+                    }
+                }
+            } label: {
+                Label("Delete", systemImage: "trash")
+            }
+            .help("Delete")
+            .disabled(selectedItem == nil)
+        }
+    }
+}
+
+struct MyShape: View {
+    var color: Color
+    var isSelected: Bool
+
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(color)
+            .overlay {
+                if isSelected {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.blue, lineWidth: 5)
+                }
+            }
     }
 }
 
